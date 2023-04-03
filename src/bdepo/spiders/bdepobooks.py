bea9b5c43a117eb66f211eaca81c1e71eab3e590
sp1thas <sp1thas@autistici.org>
2023-04-04 00:35:24 +0300

    fix currency

--- src/bdepo/spiders/bdepobooks.py
+++ src/bdepo/spiders/bdepobooks.py
@@ -151,10 +151,9 @@ class BdepobooksSpider(scrapy.Spider):
         price = response.xpath('//span[@class="sale-price"]/text()').get()
         currency = None
         if price:
-            price, currency = price.split()
-            currency = {"€": "euro"}[currency.strip()]
             try:
                 price = float(re.findall(r"\d+\.\d+", price.replace(",", "."))[0])
+                currency = "euro" if price and "€" in price else None
             except Exception as e:
                 print(e)
                 price = None
